10332e9e575f4747a61d3b02ca9ea3aad1dd4f52
Grafiel Aprilio Sitanggang <2381028@unai.edu>
2025-05-02 06:15:50 +0900

    fixing pangkat 2

--- src/utils/bookService.ts
+++ src/utils/bookService.ts
@@ -20,31 +20,31 @@ export interface CreateBookInput {
 export const bookService = {
   // Get all books
   getBooks: async () => {
-    const response = await api.get<Book[]>('/books');
+    const response = await api.get<Book[]>('/book');
     return response.data;
   },
 
   // Get a single book by ID
   getBook: async (id: string) => {
-    const response = await api.get<Book>(`/books/${id}`);
+    const response = await api.get<Book>(`/book/${id}`);
     return response.data;
   },
 
   // Create a new book
   createBook: async (bookData: CreateBookInput) => {
-    const response = await api.post<Book>('/books', bookData);
+    const response = await api.post<Book>('/book', bookData);
     return response.data;
   },
 
   // Update a book
   updateBook: async (id: string, bookData: Partial<CreateBookInput>) => {
-    const response = await api.put<Book>(`/books/${id}`, bookData);
+    const response = await api.put<Book>(`/book/${id}`, bookData);
     return response.data;
   },
 
   // Delete a book
   deleteBook: async (id: string) => {
-    const response = await api.delete(`/books/${id}`);
+    const response = await api.delete(`/book/${id}`);
     return response.data;
   },
 }; 
